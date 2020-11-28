58f52b9242041b7a8d2ccb93f4440e33395c43a1
Joao pedro Ceia <jceia@c1r1s6.42lisboa.com>
2020-11-28 16:23:44 +0000

    fixed formatting of ft_sort_int_tab.c

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 16:02:48 by jceia             #+#    #+#             */
-/*   Updated: 2020/11/28 16:06:01 by jceia            ###   ########.fr       */
+/*   Updated: 2020/11/28 16:21:02 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@ void	ft_sort_int_tab(int *tab, int size)
 	int i;
 	int j;
 	int x;
-	
+
 	i = 0;
-	while(i < size - 1)
+	while (i < size - 1)
 	{
 		j = 0;
 		while (j < size - i - 1)
 		{
-			if(tab[j] > tab[j+1])
+			if (tab[j] > tab[j + 1])
 			{
 				x = tab[j];
 				tab[j] = tab[j + 1];
